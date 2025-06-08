86e4fcf70cf18bf83a7f6e11f7a2151bcd495d9c
Abhishek Harish <itsjustabhii@gmail.com>
2025-06-08 15:43:11 +0530

    Created ExpressJS Server

--- src/app.js
+++ src/app.js
@@ -1,2 +1,14 @@
 //ExpressJS is a NodeJS framework. 
 
+const express = require('express')
+
+const app = express()
+
+//Request handler
+app.use((req,res)=>{
+    res.send('Hello from the Server')
+})
+
+app.listen(3000, () => {
+    console.log("Server is successfully listening on PORT 3000...")
+})
