d365d196646f3abedb8bfec750e7b56dd5ff03b9
Abhishek Harish <itsjustabhii@gmail.com>
2025-06-10 19:43:08 +0530

    added GET Route

--- src/app.js
+++ src/app.js
@@ -4,18 +4,18 @@ const express = require('express')
 
 const app = express()
 
-//Request handler
-app.use("/", (req,res)=>{
-    res.send('Welcome to Homepage')
+//Request handler -> Order of Routes matters in execution
+
+//app.get -> route only matches API calls with /user
+app.get("/user", (req,res)=>{
+    res.send({firstName:'Abhishek', lastName:'Harish'})
 })
 
+//app.use -> route matches with all API calls
 app.use("/test", (req,res)=>{
     res.send('Hello from the Server')
 })
 
-app.use("/hello", (req,res)=>{
-    res.send('Hello Homepage')
-})
 
 app.listen(3000, () => {
     console.log("Server is successfully listening on PORT 3000...")
