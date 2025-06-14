8660848d7563f5bd892a9e9528b8cbdfaaafb107
Abhishek Harish <itsjustabhii@gmail.com>
2025-06-15 00:00:18 +0530

    modified authorization code

--- src/app.js
+++ src/app.js
@@ -5,11 +5,25 @@ const app = express()
 //Handle Auth middleware for all GET,POST,... request
 app.use("/admin",(req,res,next) =>{
     const token = "ihjbcywdu&jwknc*$njewcn$"
-    const isAuthorised = token === "xyz"
-    if(!isAuthorised){
+    const isAdminAuthorised = token === "xyz"
+    if(!isAdminAuthorised){
         res.status(401).send("Unauthorised Request")
+    } else {
+        next()
     }
-    res.send("Route Handler 1")
+})
+
+app.get("/user",(req,res)=>{
+    res.send("User Data Sent!")
+})
+
+//Only if Authorisation completes, /admin executes
+app.get("/admin/getAllData", (req,res)=>{
+    res.send("All data sent")
+})
+
+app.get("/admin/deleteUser", (req,res)=>{
+    res.send("Delete a user!")
 })
 
 app.listen(3000, ()=>{
