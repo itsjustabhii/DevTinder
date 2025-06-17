7fd572a7152af56afb4f77a7d0013c3345b6e895
Abhishek Harish <itsjustabhii@gmail.com>
2025-06-17 23:48:52 +0530

    added Mongo connection string

--- src/config/database.js
+++ src/config/database.js
@@ -1,11 +1,9 @@
 const mongoose = require('mongoose')
 
-const connectDB = async () =>{
-    await mongoose.connect("mongodb+srv://itsjustabhii:Lewishamilton@44@devtinder.eo70w7c.mongodb.net/")
+const connectDB = async() =>{
+    await mongoose.connect("mongodb+srv://itsjustabhii:Lewishamilton@devtinder.eo70w7c.mongodb.net/")
 }
 
-connectDB().then(()=>{
-    console.log('Database connection successful!')
-}).catch(err=>{
-    console.error('Database cannot be connected')
-})
+module.exports = connectDB
+
+
