f0274b6a9f8efd98a74ce2cedfa1f84bf576e965
Abhishek Harish <itsjustabhii@gmail.com>
2025-06-22 00:13:20 +0530

    updated database password

--- src/config/database.js
+++ src/config/database.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose')
 
 const connectDB = async() =>{
-    await mongoose.connect("mongodb+srv://itsjustabhii:RPfdfgTHglGprF4r@nodetutorial.odvzbzm.mongodb.net/?retryWrites=true&w=majority&appName=NodeTutorial")
+    await mongoose.connect("mongodb+srv://itsjustabhii:RPfdfgTHglGprF4r@nodetutorial.odvzbzm.mongodb.net/?retryWrites=true&w=majority&appName=NodeTutorial/DevTinder")
 }
 
 module.exports = connectDB
