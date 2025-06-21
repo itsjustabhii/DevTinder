552ad647da89a5e7e32283f86d2922efdb095598
Abhishek Harish <itsjustabhii@gmail.com>
2025-06-21 20:36:51 +0530

    commented the tutorial CRUD op Code

--- src/app.js
+++ src/app.js
@@ -3,37 +3,37 @@ const connectDB = require("./config/database")
 const app = express()
 const {adminAuth, userAuth} = require("./middleswares/auth")
 
-//Handle Auth middleware for all GET,POST,... request
-app.use("/admin", adminAuth)
+// //Handle Auth middleware for all GET,POST,... request
+// app.use("/admin", adminAuth)
 
-app.post("/user/login",(req,res)=>{
-    res.send("User logged in successfully!")
-})
+// app.post("/user/login",(req,res)=>{
+//     res.send("User logged in successfully!")
+// })
 
-app.get("/user",userAuth, (req,res)=>{
-    res.send("User Data Sent!")
-})
+// app.get("/user",userAuth, (req,res)=>{
+//     res.send("User Data Sent!")
+// })
 
-app.get("/user/getAllData", (err,req,res,next)=>{
-    try{
-        //code to connect to DB and return back
-        throw new Error("infienfif")
-        res.send("User data sent!")
-    }
-    catch(err)
-    {
-        res.status(500).send("Something went wrong!")
-    }
-})
+// app.get("/user/getAllData", (err,req,res,next)=>{
+//     try{
+//         //code to connect to DB and return back
+//         throw new Error("infienfif")
+//         res.send("User data sent!")
+//     }
+//     catch(err)
+//     {
+//         res.status(500).send("Something went wrong!")
+//     }
+// })
 
-//Only if Authorisation completes, /admin executes
-app.get("/admin/getAllData", (req,res)=>{
-    res.send("All data sent")
-})
+// //Only if Authorisation completes, /admin executes
+// app.get("/admin/getAllData", (req,res)=>{
+//     res.send("All data sent")
+// })
 
-app.get("/admin/deleteUser", (req,res)=>{
-    res.send("Delete a user!")
-})
+// app.get("/admin/deleteUser", (req,res)=>{
+//     res.send("Delete a user!")
+// })
 
 
 connectDB().then(()=>{
