ff9be4eff7704b2d6d338bab63f6daa35d289182
Abhishek Harish <itsjustabhii@gmail.com>
2025-07-13 17:35:00 +0530

    no changes

--- src/models/user.js
+++ src/models/user.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose")
+const validator = require("validator")
 
 //Creating user schema
 const userSchema = new mongoose.Schema({
@@ -16,11 +17,21 @@ const userSchema = new mongoose.Schema({
         lowercase: true,
         required: true,
         unique: true,
-        trim: true
+        trim: true,
+        validate(value){
+            if(!validator.isEmail(value)){
+                throw new Error("Invalid email address: "+ value)
+            }
+        }
     },
     password: {
         type: String,
-        required: true
+        required: true,
+        validate(value){
+            if(!validator.isStrongPassword(value)){
+                throw new Error("Enter a strong password: "+ value)
+            }
+        }
     },
     age:{
         type: Number,
@@ -36,7 +47,12 @@ const userSchema = new mongoose.Schema({
         }
     },
     photoUrl:{
-        type:String
+        type:String,
+        validate(value){
+            if(!validator.isURL(value)){
+                throw new Error("Invalid URL address: " + value)
+            }
+        }
     },
     about:{
         type: String
