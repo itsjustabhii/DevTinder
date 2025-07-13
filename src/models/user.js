bc84dfe9bdf27e4302e618a0cee57a919ba7b23c
Abhishek Harish <itsjustabhii@gmail.com>
2025-07-13 11:53:38 +0530

    added few more user schemas

--- src/models/user.js
+++ src/models/user.js
@@ -3,22 +3,35 @@ const mongoose = require("mongoose")
 //Creating user schema
 const userSchema = new mongoose.Schema({
     firstName: {
-        type: String
+        type: String,
+        required: true
     },
     lastName: {
         type: String
     },
     emailId: {
-        type: String
+        type: String,
+        required: true,
+        unique: true
     },
     password: {
-        type: String
+        type: String,
+        required: true
     },
     age:{
         type: Number
     },
     gender:{
         type: String
+    },
+    photoUrl:{
+        type:String
+    },
+    about:{
+        type: String
+    },
+    skills:{
+        type:{String}
     }
 
 })
