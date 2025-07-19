afd7a86ecefd5c7291ab597d04e09ffd85be9043
Abhishek Harish <itsjustabhii@gmail.com>
2025-07-19 18:40:05 +0530

    Created a userAuth middlewawre which checks and validate the token received from the client everytime a request has been made

--- src/middleswares/auth.js
+++ src/middleswares/auth.js
@@ -1,3 +1,7 @@
+const jwt = require("jsonwebtoken")
+const User = require("../models/user")
+
+
 const adminAuth = (req,res,next) =>{
     const token = "ihjbcywdu&jwknc*$njewcn$"
     const isAdminAuthorised = token === "xyz"
@@ -8,14 +12,39 @@ const adminAuth = (req,res,next) =>{
     }
 }
 
-const userAuth = (req,res,next) =>{
-    const token = "ihjbcywdu&jwknc*$njewcn$"
-    const isUserAuthorised = token === "xyz"
-    if(!isUserAuthorised){
-        res.status(401).send("Unauthorised Request")
-    } else {
-        next()
+// const userAuth = (req,res,next) =>{
+//     const token = "ihjbcywdu&jwknc*$njewcn$"
+//     const isUserAuthorised = token === "xyz"
+//     if(!isUserAuthorised){
+//         res.status(401).send("Unauthorised Request")
+//     } else {
+//         next()
+//     }
+// }
+
+const userAuth = async(req, res, next) =>{
+    try {
+        //Read the token from req cookies
+    const {token} = req.cookies
+    if(!token){
+        throw new Error("Token doesn't exists!")
     }
+
+    //Validate the token
+    const decodeData = await jwt.verify(token, "DEV@Tinder$666")
+
+    //Find the User
+    const {_id} = decodeData
+    const user = await User.findById(_id)
+    if(!user){
+        throw new Error("User not found!")
+    }
+    next()
+    } catch (err) {
+        res.status(400).send("ERROR: "+ err.message)
+    }
+    
+
 }
 
 module.exports = {
