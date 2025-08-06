51d390535712526ee866e25f41c0749677637aac
Abhishek Harish <itsjustabhii@gmail.com>
2025-08-06 15:22:16 +0530

    added User import

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -1,4 +1,6 @@
 const express = require('express')
+const {validateSigUpData} = require('./utils/validation')
+const User = require("./models/user")
 
 const authRouter = express.Router() //Creating a router
 
