6fd9d3c0201015c4cfbb19166de19c86b9fdb9fd
Abhishek Harish <itsjustabhii@gmail.com>
2025-08-06 15:26:03 +0530

    added Bcrypt import

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const {validateSigUpData} = require('./utils/validation')
 const User = require("./models/user")
+const bcrypt = require("bcrypt")
 
 const authRouter = express.Router() //Creating a router
 
