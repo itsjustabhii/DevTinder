6c0ce800329cccef658ee71bb40c512e1e3ff600
Abhishek Harish <itsjustabhii@gmail.com>
2025-08-13 10:47:57 +0530

    created the connectionrequest API which takes in dynamic status and  captures user id

--- src/routes/connectRequest.js
+++ src/routes/connectRequest.js
@@ -1,10 +1,26 @@
 const express = require('express')
 const {userAuth} = require("../middleswares/auth")
 const connectionRequest = express.Router()
+const connectionRequest = require("../models/connectionRequest")
 
-connectionRequest.post("/sendConnectionRequest", userAuth, async(req, res) =>{
-    //Connection request
-    console.log("Sending a connection request")
+connectionRequest.post("/request/send/:status/:toUserId", userAuth, async(req, res) =>{
+    try {
+        const fromUserId = req.user._id
+        const toUserId = req.params.toUserId
+        const status = req.params.status
+        
+        //creating new instance
+        const connectionRequest = new ConnectionRequest({
+            fromUserId, toUserId, status
+        })
+        const data = await connectionRequest.save()
+        res.json({
+            message: "Connection request sent successfully!",
+            data
+        })
+    } catch (err) {
+        res.status(400).send("ERROR: " + err.message)
+    }
 
     res.send(user.firstName + " sent connection Request!")
 })
