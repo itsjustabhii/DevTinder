c12e83b6158bb806eb0185f3cbeef84fdb29aebc
Abhishek Harish <itsjustabhii@gmail.com>
2025-08-16 20:42:44 +0530

    added validation for to and from user

--- src/routes/connectRequest.js
+++ src/routes/connectRequest.js
@@ -1,13 +1,37 @@
 const express = require('express')
 const {userAuth} = require("../middleswares/auth")
 const connectionRequest = express.Router()
-const connectionRequest = require("../models/connectionRequest")
+const ConnectionRequest = require("../models/connectionRequest")
+const User = require("../models/user")
 
 connectionRequest.post("/request/send/:status/:toUserId", userAuth, async(req, res) =>{
     try {
         const fromUserId = req.user._id
         const toUserId = req.params.toUserId
         const status = req.params.status
+
+        //to validate the status of a profile
+        const allowedStatus = ['ignored', 'interested']
+        if(!allowedStatus.includes(status)){
+            return res.status(400).json({message:"Invalid status type: " + status})
+        }
+
+        //to check if the other user exists in DB before sending interested/ignored status
+        const toUser = await User.findbyId(toUserId)
+        if(!toUser){
+            return res.status(404).json({message: "User not found!"})
+        }
+
+        //check if a connection request already exists between two parties
+        const exisitingConnectionRequest = await ConnectionRequest.findOne({
+            $or: [
+                {fromUserId, toUserId},
+                {fromUserId:toUserId, toUserId:fromUserId},
+            ],
+        })
+        if(exisitingConnectionRequest){
+            return res.status(400).json({message: "Connection request already exists!"})
+        }
         
         //creating new instance
         const connectionRequest = new ConnectionRequest({
@@ -15,14 +39,13 @@ connectionRequest.post("/request/send/:status/:toUserId", userAuth, async(req, r
         })
         const data = await connectionRequest.save()
         res.json({
-            message: "Connection request sent successfully!",
-            data
+            message: req.user.firstName+ " is " + status + " in " + toUser.firstName,
+            data,
         })
     } catch (err) {
         res.status(400).send("ERROR: " + err.message)
     }
 
-    res.send(user.firstName + " sent connection Request!")
 })
 
 module.exports = connectionRequest
