22be8e8bcdfaf1162092da7c5cc6526550a50b6a
Abhishek Harish <itsjustabhii@gmail.com>
2025-08-30 15:25:22 +0530

    populating based on userID

--- src/routes/user.js
+++ src/routes/user.js
@@ -10,7 +10,7 @@ userRouter.get("/user/requests/received", userAuth, async(req,res) =>{
         const connectionRequests = await connectionRequest.find({
             toUserId: loggedInUser._id,
             status: "interested"
-        })
+        }).populate("fromUserId", ["firstName", "lastName", "about", "age", "photoUrl", "gender", "skills"]) //maps the user ID to user name and displayed the fields written in array
     } catch (err) {
         req.statusCode(400).send("ERROR: " + err.message)
         
